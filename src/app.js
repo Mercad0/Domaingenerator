0a00c3fd4d8892e224c491ff1cc038912cea63ba
Mercad0 <abimaelmercado05@gmail.com>
2022-10-07 22:08:31 +0000

    domain generator

--- src/app.js
+++ src/app.js
@@ -2,10 +2,31 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+// import "./assets/img/rigo-baby.jpg";
+// import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  // let domainName = [""];
+  let pronoun = ["arcade", "dedicated", "cool"];
+  let adj = ["fruity", "fanbase", "cheap"];
+  let noun = ["shoes", "backpack", "frenchtoast", "socks"];
+  let domain = [".com", ".net", ".io", ".org", ".biz"];
+
+  function domainReturn() {
+    document.getElementById("domainReturn").innerHTML =
+      pronoun[Math.floor(Math.random() * pronoun.length)] +
+      adj[Math.floor(Math.random() * adj.length)] +
+      noun[Math.floor(Math.random() * noun.length)] +
+      domain[Math.floor(Math.random() * domain.length)];
+  }
+  for (let a = 0; a < pronoun.length; a++) {
+    for (let b = 0; b < adj.length; b++) {
+      for (let c = 0; c < noun.length; c++) {
+        for (let d = 0; d < domain.length; d++) {}
+      }
+    }
+  }
+
+  document.getElementById("btn1").addEventListener("click", domainReturn);
 };
